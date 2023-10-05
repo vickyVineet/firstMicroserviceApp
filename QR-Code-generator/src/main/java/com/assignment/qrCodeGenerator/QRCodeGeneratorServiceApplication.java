867ddcf453aefab10b58998e55f7c4e3fa04ef25
vickyVineet <32819034+vickyVineet@users.noreply.github.com>
2023-10-05 14:34:27 +0530

    Add files via upload

---
+++ QR-Code-generator/src/main/java/com/assignment/qrCodeGenerator/QRCodeGeneratorServiceApplication.java
@@ -0,0 +1,14 @@
+package com.assignment.qrCodeGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@EnableDiscoveryClient
+public class QRCodeGeneratorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QRCodeGeneratorServiceApplication.class, args);
+	}
+
+}
